2b1b3540b58c1099f4406a6a59e7b370135a7fd3
github honganji <yujicavs@icloud.com>
2023-04-15 19:37:28 +0900

    add test for check if token contract role is passed correctly

--- test/test.ts
+++ test/test.ts
@@ -37,8 +37,12 @@ describe('ETH-DAO test', function () {
     new ethers.Wallet(PRIVATE_KEY!, ethers.getDefaultProvider(ALCHEMY_API_URL)),
   );
 
+  // 1-initialize-sdk.tsのテスト
   it('sdk is working', async function () {
+    // sdkからアドレスを取得
     const address = await sdk.getSigner()?.getAddress();
+
+    // sdkを初期化したアドレスが自分のウォレットアドレスと一致しているか確認
     assert.equal(address, WALLET_ADDRESS);
   });
 
@@ -47,71 +51,111 @@ describe('ETH-DAO test', function () {
   const token = sdk.getContract(ERCTokenAddress, 'token');
   const vote = sdk.getContract(gavananceAddress, 'vote');
 
-  //   it('metadata is set correctly', async function () {
-  //     // メタデータを取得
-  //     const metadata = await (await editionDrop).metadata.get();
+  // 2-deploy-drop.tsのテスト
+  it('metadata is set', async function () {
+    // メタデータを取得
+    const metadata = await (await editionDrop).metadata.get();
 
-  //     // メタデータがsetされているかテスト
-  //     assert.notEqual(metadata, null);
+    // メタデータがsetされているかテスト
+    assert.notEqual(metadata, null);
 
-  //     // メタデータの内容の一部が一致しているかチェック
-  //     assert.equal(metadata.fee_recipient, AddressZero);
-  //   });
+    // メタデータの内容の一部が一致しているかチェック
+    assert.equal(metadata.fee_recipient, AddressZero);
+  });
 
-  //   it('NFT is minted', async function () {
-  //     const NFTInfo = await (await editionDrop).get(0);
-  //     assert.notEqual(NFTInfo, null);
-  //   });
+  // 3-config-nft.tsのテスト
+  it('NFT is minted', async function () {
+    // 最初にmintされたNFTの情報を取得する
+    const NFTInfo = await (await editionDrop).get(0);
 
-  //   it('NFT condition is set correctly', async function () {
-  //     const condition = await (
-  //       await editionDrop
-  //     ).erc1155.claimConditions.getActive('0');
-  //     assert.equal(condition.price.toNumber(), 0);z
-  //   });
+    // NFTの情報が空ではないことを確認する
+    assert.notEqual(NFTInfo, null);
+  });
 
-  //   it('token is minted correctly', async function () {
-  //     const tokenInfo = await (await token).balance();
-  //     assert.equal(tokenInfo.symbol, 'TSC');
-  //   });
+  // 4-set-claim-condition.tsのテスト
+  it('NFT condition is set', async function () {
+    // トークンに与えられた条件を取得する
+    const condition = await (
+      await editionDrop
+    ).erc1155.claimConditions.getActive('0');
 
-  //   it('token is minted correctly', async function () {
-  //     const tokenInfo = await (await token).totalSupply();
-  //     assert.equal(tokenInfo.displayValue, '1000000.0');
-  //   });
+    // 条件として与えられたものの一つであるNFTの価格が0であることを確認する
+    assert.equal(condition.price.toNumber(), 0);
+  });
 
-  //   it('token is transfered', async function () {
-  //     const balance = await (await token).balanceOf(ownerWalletAddress);
-  //     const fixedBalance = Number(balance.value).toString();
-  //     console.log(fixedBalance);
-  //     assert.equal(fixedBalance, '1e+22');
-  //   });
+  // 5-deploy-token.tsのテスト
+  it('token contract is deployed', async function () {
+    // トークンに与えられた情報を取得する
+    const tokenInfo = await (await token).balance();
 
-  //   it('vote contract has 90 % of owners token', async function () {
-  //     const metadata = await (await vote).metadata.get();
-  //     assert.equal(metadata.name, 'My amazing DAO');
-  //   });
+    // トークンのシンボルがTSCとなっているか確認する
+    assert.equal(tokenInfo.symbol, 'TSC');
+  });
 
-  //   it('vote contract has 90 % of owners token', async function () {
-  //     // ウォレットのトークン残高を取得します
-  //     const ownedTokenBalance = (
-  //       await (await token).balanceOf(ownerWalletAddress)
-  //     ).value;
+  // 6-print-money.tsのテスト
+  it('token is minted', async function () {
+    // トークンの情報を取得する
+    const tokenInfo = await (await token).totalSupply();
 
-  //     // ウォレットのトークン残高を取得します
-  //     const contractTokenBalance = (
-  //       await (await token).balanceOf((await vote).getAddress())
-  //     ).value;
-  //     assert.equal(Number(ownedTokenBalance), Number(contractTokenBalance) / 9);
-  //   });
+    // トークンの合計が1e+24となっているか確認する
+    assert.equal(Number(tokenInfo.value).toString(), '1e+24');
+  });
 
+  // 7-airdrop-token.tsのテスト
+  it('token is transfered', async function () {
+    // このコントラクトのオーナーに与えられているトークンの合計を確認する
+    const balance = await (await token).balanceOf(ownerWalletAddress);
+
+    // コントラクトのトークンの合計を10進数に変換する
+    const fixedBalance = Number(balance.value).toString();
+
+    // コントラクトのトークンの合計が1e+22となっているか確認
+    assert.equal(fixedBalance, '1e+22');
+  });
+
+  // 8-deploy-vote.tsのテスト
+  it('vote contract has right info', async function () {
+    // 投票コントラクトのメタデータを取得
+    const metadata = await (await vote).metadata.get();
+
+    // 投票コントラクトに正しく情報が入っているか確認
+    assert.equal(metadata.name, 'My amazing DAO');
+  });
+
+  // 9-setup-vote.tsのテスト
+  it('vote contract has as 9 times much tokens as owner has', async function () {
+    // ウォレットのトークン残高を取得します
+    const ownedTokenBalance = (
+      await (await token).balanceOf(ownerWalletAddress)
+    ).value;
+
+    // ウォレットのトークン残高を取得します
+    const contractTokenBalance = (
+      await (await token).balanceOf((await vote).getAddress())
+    ).value;
+
+    // オーナーが所有するトークンの9倍のトークン量をコントラクトが所有していることを確認
+    assert.equal(Number(ownedTokenBalance) * 9, Number(contractTokenBalance));
+  });
+
+  // 10-create-vote-proposals.tsのテスト
   it('vote contract has proposal', async function () {
     // 投票コントラクトに挙げられた提案を取得します
     const proposal = (await (await vote).getAll())[0];
 
+    // 投票コントラクトへ提案がされているか確認する
     assert.equal(
       proposal.description,
       'Should the DAO mint an additional 420000 tokens into the treasury?',
     );
+  });
+
+  // 11-revoke-roles.tsのテスト
+  it('token role is passed to contract', async function () {
+    // 投票コントラクトに挙げられた提案を取得します
+    const roles = await (await token).roles.getAll();
+
+    // adminの権限が誰にも与えられていないlことを確認する。
+    assert.equal(roles.admin, [].toString());
   });
 });
