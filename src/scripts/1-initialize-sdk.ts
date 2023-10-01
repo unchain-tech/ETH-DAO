de0c0f4c3dd655a40d26d50262ece99870ff5bc5
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-10-01 22:26:08 +0900

    fix: lint no-unused-expressions

--- src/scripts/1-initialize-sdk.ts
+++ src/scripts/1-initialize-sdk.ts
@@ -3,25 +3,26 @@ import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 
 // 環境変数を env ファイルから読み込む
 const { loadEnvConfig } = nextEnv;
-const projectDir = process.cwd();
-loadEnvConfig(projectDir).combinedEnv;
+const { PRIVATE_KEY, CLIENT_ID, SECRET_KEY } = loadEnvConfig(
+  process.cwd(),
+).combinedEnv;
 
 // 環境変数が取得できてとれているか確認
-if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === '') {
+if (!PRIVATE_KEY || PRIVATE_KEY === '') {
   console.log('🛑 Private key not found.');
 }
 
-if (!process.env.CLIENT_ID || process.env.CLIENT_ID === '') {
+if (!CLIENT_ID || CLIENT_ID === '') {
   console.log('🛑 Client ID of API Key not found.');
 }
 
-if (!process.env.SECRET_KEY || process.env.SECRET_KEY === '') {
+if (!SECRET_KEY || SECRET_KEY === '') {
   console.log('🛑 Secret Key of API Key not found.');
 }
 
-const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY!, 'sepolia', {
-  clientId: process.env.CLIENT_ID,
-  secretKey: process.env.SECRET_KEY,
+const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY!, 'sepolia', {
+  clientId: CLIENT_ID,
+  secretKey: SECRET_KEY,
 });
 
 // ここでスクリプトを実行
